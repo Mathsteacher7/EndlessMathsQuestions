54be96df1fc68731ddcf9696a3a08e75f64d66c1
Daniel H Cohen <hofcohen@gmail.com>
2020-01-05 22:28:42 +0000

    grammar in Home Page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
     <section className="section">
       <div className="container home">
         <h1 data-testid="homeTitle">Welcome to Endless Maths Questions</h1>
-        <h2>The place to skill up your maths and practise endlessly, untill you will be sure that you are on the top of the topic!</h2> 
+        <h2>The place to skill up your maths and practise endlessly, until you are sure that you are on the top of the topic!</h2> 
         <p>The idea of Endless Maths is to give you the option to practice a specific skill you want. 
-          <br/> For that purpose I created this website that you could solve a lot of exercise in a very specific topic. 
-          <br/>As time will go by, I will add more and more topics, If you feel you want a specific topic, feel free to write me on my &nbsp; 
+          <br/> For that purpose, I created this website that you could solve a lot of exercise in a particular topic. 
+          <br/>As time will go by, I will add more and more topics, If you feel you want a specific topic, please write to me on my &nbsp; 
           <a href="https://www.linkedin.com/in/daniel-h-cohen/">LinkedIn</a>
         </p>
         <img src="https://i.imgur.com/u07MRsz.jpg"></img>
