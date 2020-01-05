b1f04c0e76bc031c1a23a08b96ae6cf2c1f32eda
Daniel H Cohen <hofcohen@gmail.com>
2020-01-05 22:30:59 +0000

    style issues in Home Page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <section className="section">
       <div className="container home">
-        <h1 data-testid="homeTitle">Welcome to Endless Maths Questions</h1>
-        <h2>The place to skill up your maths and practise endlessly, until you are sure that you are on the top of the topic!</h2> 
+        <h1 className="title" data-testid="homeTitle">Welcome to Endless Maths Questions</h1>
+        <h2 className="subtitle">The place to skill up your maths and practise endlessly, until you are sure that you are on the top of the topic!</h2> 
         <p>The idea of Endless Maths is to give you the option to practice a specific skill you want. 
           <br/> For that purpose, I created this website that you could solve a lot of exercise in a particular topic. 
           <br/>As time will go by, I will add more and more topics, If you feel you want a specific topic, please write to me on my &nbsp; 
